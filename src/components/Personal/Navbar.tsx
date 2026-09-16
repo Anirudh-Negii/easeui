3f4b1e48588eb958d0578ef636dbfeecfbfb718b
Anirudh <anirudh.formal88@gmail.com>
2026-09-16 14:22:47 +0530

    Clean up navbar navigation

--- src/components/Personal/Navbar.tsx
+++ src/components/Personal/Navbar.tsx
@@ -1,5 +1,5 @@
 import { toggleTheme } from "@/features/ThemeSlice";
-import { Moon, Search, Sun } from "lucide-react";
+import { Moon, Sun } from "lucide-react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 
@@ -7,7 +7,7 @@ const Navbar = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { mode } = useSelector(
-    (state: { theme: { mode: string } }) => state.theme
+    (state: { theme: { mode: string } }) => state.theme,
   );
   console.log("this is theme->", mode);
 
@@ -18,17 +18,8 @@ const Navbar = () => {
           onClick={() => navigate("/")}
           className="font-bold text-2xl cursor-pointer"
         >
-          EaseUi
+          EaseUI
         </h1>
-
-        <div className="hidden sm:flex items-center bg-transparent rounded-md px-3 py-1.5 shadow-xs shadow-gray-300 border border-gray-200">
-          <Search size={18} className="text-gray-500" />
-          <input
-            type="text"
-            placeholder="Search components"
-            className="ml-2 bg-transparent outline-none text-sm text-gray-700 placeholder-gray-400"
-          />
-        </div>
       </div>
 
       <ul className="hidden md:flex items-center gap-6 text-gray-500">
@@ -38,8 +29,12 @@ const Navbar = () => {
         >
           Components
         </li>
-        <li className="cursor-pointer hover:text-black">About</li>
-        <li className="cursor-pointer hover:text-black">Templates</li>
+        <li
+          onClick={() => navigate("showcase")}
+          className="cursor-pointer hover:text-black"
+        >
+          Showcase
+        </li>
         {mode === "dark" && (
           <li
             className="cursor-pointer p-2 rounded-full hover:bg-gray-100 dark:hover:bg-gray-800"
@@ -57,9 +52,6 @@ const Navbar = () => {
           </li>
         )}
       </ul>
-
-      {/* Mobile Hamburger */}
-      <button className="md:hidden text-gray-700">☰</button>
     </nav>
   );
 };
